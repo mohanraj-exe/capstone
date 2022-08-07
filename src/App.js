f81a7242bc2e6ff3496ff24e73e95d434518acbe
mohanraj-exe <smohan1029@gmail.com>
2022-08-07 16:17:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productList" element={<ProductList />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://m.media-amazon.com/images/I/51TYUIcF2EL._UL1500_.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://m.media-amazon.com/images/I/71IfKmKffPL._UL1500_.jpg",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://m.media-amazon.com/images/I/61vK52TwjzL._UL1500_.jpg",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://m.media-amazon.com/images/I/61vK52TwjzL._UL1500_.jpg",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://m.media-amazon.com/images/I/51TYUIcF2EL._UL1500_.jpg",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://m.media-amazon.com/images/I/61n68PbiGEL._UL1500_.jpg",
+      title: "LIGHT T-SHIRTS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://m.media-amazon.com/images/I/81uZMuO+gaS._UL1500_.jpg",
+    },
+    {
+      id:2,
+      img:"https://m.media-amazon.com/images/I/71IfKmKffPL._UL1500_.jpg",
+    },
+    {
+      id:3,
+      img:"https://m.media-amazon.com/images/I/81xA4AlgTpL._AC_UY580_FMwebp_.jpg",
+    },
+    {
+      id:4,
+      img:"https://m.media-amazon.com/images/I/71IfKmKffPL._UL1500_.jpg",
+    },
+    {
+      id:5,
+      img:"https://m.media-amazon.com/images/I/61vK52TwjzL._UL1500_.jpg",
+    },
+    {
+      id:6,
+      img:"https://m.media-amazon.com/images/I/61n68PbiGEL._UL1500_.jpg",
+    },
+    {
+      id:7,
+      img:"https://m.media-amazon.com/images/I/51TYUIcF2EL._UL1500_.jpg",
+    },
+    {
+      id:8,
+      img:"https://m.media-amazon.com/images/I/61DBKKMHTWL._UL1500_.jpg",
+    },
+  ]
